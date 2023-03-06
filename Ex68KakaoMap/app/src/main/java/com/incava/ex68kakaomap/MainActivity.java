d2d9516277ad295a2cdf7668ae36735705f57521
incava <ingi1118@naver.com>
2023-03-06 15:09:03 +0900

    2023-03-06

--- Ex68KakaoMap/app/src/main/java/com/incava/ex68kakaomap/MainActivity.java
+++ Ex68KakaoMap/app/src/main/java/com/incava/ex68kakaomap/MainActivity.java
@@ -3,6 +3,14 @@ package com.incava.ex68kakaomap;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ViewGroup;
+
+import com.kakao.util.maps.helper.Utility;
+
+import net.daum.mf.map.api.MapPOIItem;
+import net.daum.mf.map.api.MapPoint;
+import net.daum.mf.map.api.MapView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String keyHash = Utility.getKeyHash(this);
+        Log.i("keyHash:",keyHash);
+        double lat =37.5617;
+        double lng =127.0344;
+        MapView mapView = new MapView(this);
+        ViewGroup mapViewContainer = findViewById(R.id.map_view);
+        MapPOIItem marker = new MapPOIItem();
+        marker.setItemName("Default Marker");
+        marker.setTag(0);
+        mapView.setMapCenterPoint(MapPoint.mapPointWithGeoCoord(lat, lng), true);
+        marker.setMapPoint(MapPoint.mapPointWithGeoCoord(lat,lng));
+        marker.setMarkerType(MapPOIItem.MarkerType.BluePin); // 기본으로 제공하는 BluePin 마커 모양.
+        marker.setSelectedMarkerType(MapPOIItem.MarkerType.RedPin); // 마커를 클릭했을때, 기본으로 제공하는 RedPin 마커 모양.
+        mapView.addPOIItem(marker);
+        mapViewContainer.addView(mapView);
     }
 }
